35c4638874453183a31ec4f451267b4c048400a9
yzh231 <yzh231@uclive.ac.nz>
2017-05-21 17:12:22 +1200

    adsafds

--- ass2.py
+++ ass2.py
@@ -2,6 +2,8 @@ from subprocess import check_output,call
 import argparse
 import sys
 
+NK=3.0
+
 def get_nodes(symbol,N):
     nodes = ["{}{}".format(symbol,n) for n in range(1, N + 1)]
     return nodes
@@ -28,6 +30,7 @@ def main():
     transitNodes = get_nodes('T',Y)
     endNodes = get_nodes('D',Z)
     everyNode = set()
+    u_set = set()
     links = []
 
     print("startNodes",startNodes)
@@ -50,6 +53,25 @@ def main():
             eqn += "={}".format(i+j)
             output_lp_file.write(eqn + "\n")
 
+    output_lp_file.write("3flowConstraints:\n")
+    for i in range(1, X + 1):
+        for j in range(1, Z + 1):
+            eqn = ""
+            for t in transitNodes:
+                eqn += "u{}{}{}+".format(startNodes[i - 1], t, endNodes[j - 1])
+                u_set.add("u{}{}{}".format(startNodes[i - 1], t, endNodes[j - 1]))
+            eqn = eqn[:-1]
+            eqn += "={}".format(NK)
+            output_lp_file.write(eqn + "\n")
+
+    output_lp_file.write("equalFlowConstraints:\n")
+    for i in range(1, X + 1):
+        for j in range(1, Z + 1):
+            eqn = ""
+            for t in transitNodes:
+                eqn = "3x{}{}{}={}u{}{}{}".format(startNodes[i - 1], t, endNodes[j - 1],i+j,startNodes[i - 1], t, endNodes[j - 1])
+            output_lp_file.write(eqn + "\n")
+
 
     output_lp_file.write("linkConstraints:\n")
     for i in range(1,X+1):
@@ -59,7 +81,7 @@ def main():
                 eqn += "x{}{}{}+".format(startNodes[i-1], transitNodes[j-1], endNodes[k-1])
                 everyNode.add("x{}{}{}".format(startNodes[i-1], transitNodes[j-1], endNodes[k-1]))
             eqn = eqn[:-1]
-            eqn += "-C{}{}r <=0".format(i,j)
+            eqn += "-c{}{} <=0".format(i,j)
             links.append("y{}{}".format(startNodes[i-1], transitNodes[j-1]))
             output_lp_file.write(eqn + "\n")
 
@@ -71,14 +93,31 @@ def main():
                 eqn += "x{}{}{}+".format(startNodes[i-1], transitNodes[k-1], endNodes[j-1])
                 everyNode.add("x{}{}{}".format(startNodes[i-1], transitNodes[k-1], endNodes[j-1]))
             eqn = eqn[:-1]
-            eqn += "-C{}{}r <= 0".format(k,j)
+            eqn += "-D{}{} <= 0".format(k,j)
             links.append("y{}{}".format(transitNodes[k-1], endNodes[j-1]))
             output_lp_file.write(eqn + "\n")
+
+    output_lp_file.write("transiteNodesConstraint:\n")
+    for i in transitNodes:
+        eqn = ""
+        for j in startNodes:
+
+            for d in endNodes:
+                eqn += "x{}{}{}+".format(j,i, d)
+        eqn = eqn[:-1]
+        eqn += "-r <= 0"
+        output_lp_file.write(eqn + "\n")
 
     output_lp_file.write("negativityConstraints:\n")
     for i in list(everyNode):
         output_lp_file.write("{} >= 0\n".format(i))
+
     output_lp_file.write("r >= 0\n")
+
+    output_lp_file.write("Binary\n")
+    for u in list(u_set):
+        output_lp_file.write(u+"\n")
+
     output_lp_file.write("END\n")
     output_lp_file.close()
 
