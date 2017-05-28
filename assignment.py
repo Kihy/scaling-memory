9061728767e5bd81d5edef747cd992c4c423b36b
Ke He <khe60@uclive.ac.nz>
2017-05-28 15:46:09 +1200

    asdf

--- assignment.py
+++ assignment.py
@@ -1,10 +1,12 @@
 from scriptGenerator import generate_lp
 
+
 def main():
-    result_file=open("result","w")
-    for y in range(3,8):
-        max_load,numLinks,highest_capcity=generate_lp(7,y,7)
-        result_file.write("{} {} {} {}\n".format(y,max_load,numLinks,highest_capcity))
+    """function to fix X=7,Y from 3 to 7, and Z=7"""
+    result_file = open("result", "w")
+    for y in range(3, 8):
+        max_load, numLinks, highest_capcity = generate_lp(7, y, 7)
+        result_file.write("{} {} {} {}\n".format(y, max_load, numLinks, highest_capcity))
     result_file.close()
 
 
